caaa274454db64d5b3884c3dab529c3a11c193e8
Robert Leitner <robertleitner2321@gmail.com>
2022-08-15 00:59:00 +0200

    # Creation of Auth0 security features

---
+++ backend/src/main/java/com/robertleitner/movieenjoy/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.robertleitner.movieenjoy.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+    }
+}
